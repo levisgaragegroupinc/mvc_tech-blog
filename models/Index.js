1ef53e415ba8cfeccc0bb30dfc8dad755614b761
Travis Bates <tbateswsu@gmail.com>
2022-05-21 16:16:41 -0700

    Added possible associations between User, Post and Comment to index file. Need to add foreign key definitions once defined

---
+++ models/Index.js
@@ -0,0 +1,19 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// Table associations
+
+User.hasMany(Post, {});
+
+Post.belongsTo(User, {});
+
+Comment.belongsTo(User, {});
+
+Comment.belongsTo(Post, {});
+
+User.hasMany(Comment, {});
+
+Post.hasMany(Comment, {});
+
+module.exports = { User, Post, Comment };
