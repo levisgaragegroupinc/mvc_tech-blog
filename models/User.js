f25354d8d992cd8ced1cd870b6982e4bae920f15
Travis Bates <tbateswsu@gmail.com>
2022-05-21 16:41:08 -0700

    added model structure for User model. Need to fill in hooks for hashing user password

--- models/User.js
+++ models/User.js
@@ -4,7 +4,31 @@ const sequalize = require("../config/connection");
 class User extends Model {}
 
 User.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: /^[0-9a-f]{64}$/i,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5],
+      },
+    },
+  },
+  { hooks: {} },
+
   {
     sequelize,
     freezeTableName: true,
