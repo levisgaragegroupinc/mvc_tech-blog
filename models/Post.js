81f24e3124f1082b06ae37fa4631472a61e2f5bd
Travis Bates <tbateswsu@gmail.com>
2022-05-21 16:32:41 -0700

    Added model structure for Post

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,32 @@ const sequalize = require("../config/connection");
 class Post extends Model {}
 
 Post.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_txt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
   {
     sequelize,
     freezeTableName: true,
