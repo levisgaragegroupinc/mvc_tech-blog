1adb929fb563b5e8d623fc1e1ab8c87a2d8623d9
Travis Bates <tbateswsu@gmail.com>
2022-05-22 13:23:04 -0700

    Get single post route added to post-routes. Template name needs to be populated with name of actual template.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -28,3 +28,37 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// GET SINGLE POST BY ID
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "created_at", "post_txt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "post_id", "comment_txt", "created_at"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    if (!postData) {
+      res
+        .status(404)
+        .json({ message: `No post with ID ${req.params.id} found.` });
+    }
+
+    res.render("nameoftemplate_here", {
+      postData,
+    });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
