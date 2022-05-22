d9b09240a18cfcd0eadac3ea8c4934129545256a
Travis Bates <tbateswsu@gmail.com>
2022-05-22 14:38:45 -0700

    added some associations in the models index file. Revisit later to make sure all the appropriate associations are present

--- models/Index.js
+++ models/Index.js
@@ -4,16 +4,28 @@ const Comment = require("./Comment");
 
 // Table associations
 
-User.hasMany(Post, {});
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
 
-Post.belongsTo(User, {});
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
-Comment.belongsTo(User, {});
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
-Comment.belongsTo(Post, {});
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
 
-User.hasMany(Comment, {});
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
 
-Post.hasMany(Comment, {});
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
 
 module.exports = { User, Post, Comment };
