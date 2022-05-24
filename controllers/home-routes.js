d1efc8218ff0db3050f37c278728e1048bf1b2a4
Travis Bates <tbateswsu@gmail.com>
2022-05-23 18:20:15 -0700

    pseudo code outline of needed routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
 const { Comment } = require("../models");
 const sequelize = require("../config/connection");
+
+// GET ALL POSTS AND RENDER
+
+// GET LOGIN AND RENDER LOGIN
+
+// GET SIGNUP AND RENDER SIGNUP
+
+// GET ONE POST BY ID AND RENDER SINGLE POST
+
+//
+
+module.exports = router;
