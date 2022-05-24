3d33c02204658a5577308519902da2baee97250f
Travis Bates <tbateswsu@gmail.com>
2022-05-24 08:05:42 -0700

    feat: added get all user posts for logged in user on dashboard route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,8 +2,39 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const sequelize = require("../config/connection");
+const withAuth = require("../../utils/auth");
 
 // GET ALL POSTS AND RENDER
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const allUserPosts = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: ["id", "title", "created_at", "post_txt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "post_id", "comment_txt", "created_at"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    if (!allUserPosts) {
+      res.status(404).json({ message: `No posts found.` });
+    }
+    const userPosts = allUserPosts.map((post) => {
+      post.get({ plain: true });
+    });
+    res.render("dashboard", { userPosts, loggedIn: true });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // GET ONE POST AND RENDER
 
