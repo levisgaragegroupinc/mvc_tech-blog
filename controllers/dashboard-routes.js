134642ecdf60302743902df8fb9920535eca8120
Travis Bates <tbateswsu@gmail.com>
2022-05-24 08:12:49 -0700

    feat: added get single post to dashboard-routes for edit post template

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -37,6 +37,40 @@ router.get("/", withAuth, async (req, res) => {
 });
 
 // GET ONE POST AND RENDER
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "created_at", "post_txt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "post_id", "comment_txt", "created_at"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    if (!postData) {
+      res
+        .status(404)
+        .json({ message: `No post with ID ${req.params.id} found.` });
+    }
+    const post = postData.get({ plain: true });
+
+    res.render("editposttemplate_name", {
+      post,
+      loggedIn: true,
+    });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // GET CREATE NEW POST AND RENDER
 
