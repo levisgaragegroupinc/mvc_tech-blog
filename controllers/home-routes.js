fc216e8f7885df35abfa3dfe36d2122db9781da8
Travis Bates <tbateswsu@gmail.com>
2022-05-24 08:40:15 -0700

    feat: get all posts route added to home route.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,36 @@ const sequelize = require("../config/connection");
 const withAuth = require("../../utils/auth");
 
 // GET ALL POSTS AND RENDER
+router.get("/", async (req, res) => {
+  try {
+    const allPostData = await Post.findAll({
+      attributes: ["id", "title", "created_at", "post_txt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "post_id", "comment_txt", "created_at"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    if (!allPostData) {
+      res.status(404).json({ message: `No posts found.` });
+    }
+    const posts = allPostData.map((post) => {
+      post.get({ plain: true });
+    });
+    res.render("homepagetemplate_name", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // GET LOGIN AND RENDER LOGIN
 
