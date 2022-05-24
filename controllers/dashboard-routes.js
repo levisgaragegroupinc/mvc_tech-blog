a6417ce1f60354c0a3fb710e6a0f7bfa0b330420
Travis Bates <tbateswsu@gmail.com>
2022-05-23 18:26:36 -0700

    added pseudo code to dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,11 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const sequelize = require("../config/connection");
+
+// GET ALL POSTS AND RENDER
+
+// GET ONE POST AND RENDER
+
+// GET CREATE NEW POST AND RENDER
+
+module.exports = router;
