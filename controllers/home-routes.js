b4f6dd5bcaee45c8f2d6b3277bd063e1616d35a9
Travis Bates <tbateswsu@gmail.com>
2022-05-24 08:49:58 -0700

    feat: added route to get a single post and render.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -54,7 +54,38 @@ router.get("/signup", (req, res) => {
 });
 
 // GET ONE POST BY ID AND RENDER SINGLE POST
-
-//
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "created_at", "post_txt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "user_id", "post_id", "comment_txt", "created_at"],
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    if (!postData) {
+      res
+        .status(404)
+        .json({ message: `No post with ID ${req.params.id} found.` });
+    }
+    const post = postData.get({ plain: true });
+    res.render("singleposttemplate_name", {
+      post,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
