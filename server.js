2f3a07357ee6a8ee80f726212a005d7a8ddee876
Travis Bates <tbateswsu@gmail.com>
2022-05-27 13:16:47 -0700

    added sequelize store const to server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const path = require("path");
 const express = require("express");
-
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +14,9 @@ const sess = {
   secret: "Super secret secret",
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
