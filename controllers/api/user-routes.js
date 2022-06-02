f7c31b6d747cacd3b24e1495475b7a18465630c0
Travis Levi B <tbateswsu@gmail.com>
2022-06-02 11:34:56 -0700

    Bug: the sign-form and login-form classes were on the wrong div element. Moved them to the form element.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
-const withAuth = require("../../utils/auth");
+// const withAuth = require("../../utils/auth");
 
 // GET ALL USERS ROUTE. /api/users
 router.get("/", async (req, res) => {
