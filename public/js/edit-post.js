06dfe34a52f2f161a1aa624a19bf445f33ea8be2
Travis Levi B <tbateswsu@gmail.com>
2022-06-13 16:57:49 -0700

    Bug: fixed update post route. Changed from POST to PUT on the API route.

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,23 +1,16 @@
 const editPostForm = async (event) => {
   event.preventDefault();
-
   const title = document.querySelector('textarea[name="title"]').value;
-  console.log(title);
-
   const post_txt = document.querySelector(
     'textarea[name="post-content"]'
   ).value;
-  console.log(post_txt);
-
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
-  console.log(id);
-
   if (title && post_txt && id) {
     const response = await fetch("/api/posts/" + id, {
-      method: "POST",
-      body: JSON.stringify({ title: title, post_txt: post_txt }), //added title: title, and post_txt: post_txt
+      method: "PUT",
+      body: JSON.stringify({ title: title, post_txt: post_txt }),
       headers: { "Content-Type": "application/json" },
     });
 
