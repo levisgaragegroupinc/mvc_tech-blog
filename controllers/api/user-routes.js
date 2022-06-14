bb2a4e1a5a9f027bfb184fda6ee798a6f22344a3
Travis Bates <tbateswsu@gmail.com>
2022-06-13 18:05:44 -0700

    added slash on end of models/

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, User, Comment } = require("../../models");
+const { Post, User, Comment } = require("../../models/");
 
 // GET ALL USERS ROUTE. /api/users
 router.get("/", async (req, res) => {
